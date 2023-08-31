832ddbb33f511fe17f270534fdf5408a4b74c6e2
Gabriel Fernandes <contato.gfernandesribeiro@gmail.com>
2023-08-30 21:48:45 -0300

    [#1] improve sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,7 +7,14 @@ export const Sidebar = () => {
     <div className="w-1/4 bg-zinc-900 sticky min-h-screen">
       <div className="fixed w-1/4">
         <nav className="w-full p-4">
-          <SidebarItem name="Hiragana" />
+          <SidebarItem
+            name="Hiragana"
+            subitems={["Hiragana - Part 01", "Hiragana Flashcards 1"]}
+          />
+          <SidebarItem
+            name="Katakana"
+            subitems={["Katakana - Part 01", "Katakana Flashcards 1"]}
+          />
         </nav>
       </div>
     </div>
--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -4,39 +4,53 @@ import { useState } from "react";
 
 type Props = {
   name: string;
+  subitems: string[];
 };
 
-export const SidebarItem = ({ name }: Props) => {
+export const SidebarItem = ({ name, subitems }: Props) => {
   const [active, setActive] = useState(false);
 
   return (
-    <div
-      className={
-        (active ? "bg-zinc-800 " : "") +
-        "p-3 w-full hover:bg-zinc-800 rounded cursor-pointer flex"
-      }
-      onClick={() => setActive((prev) => !prev)}
-    >
-      <div>{name}</div>
+    <div className="w-full">
       <div
         className={
-          (active ? "rotate-90 " : "") + "ml-auto duration-300 ease-in-out"
+          (active ? "bg-zinc-800 " : "") +
+          "p-3 w-full hover:bg-zinc-800 rounded cursor-pointer flex"
         }
+        onClick={() => setActive((prev) => !prev)}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
+        <div>{name}</div>
+        <div
+          className={
+            (active ? "rotate-90 " : "") + "ml-auto duration-300 ease-in-out"
+          }
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M8.25 4.5l7.5 7.5-7.5 7.5"
-          />
-        </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8.25 4.5l7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </div>
+      </div>
+
+      <div className={(active ? "flex flex-col " : "hidden ") + "ml-3"}>
+        {subitems.map((subitem, index) => (
+          <div
+            className="py-2 mt-2 rounded hover:bg-zinc-800 w-full cursor-pointer px-3"
+            key={index}
+          >
+            {subitem}
+          </div>
+        ))}
       </div>
     </div>
   );
