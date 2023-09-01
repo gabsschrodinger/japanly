07fe2cc244701398a531a70114fc3692b12113fa
Gabriel Fernandes <contato.gfernandesribeiro@gmail.com>
2023-09-01 00:17:45 -0300

    [#3] add text pre-flashcard on lesson 2

--- app/lessons/lesson-2/page.tsx
+++ app/lessons/lesson-2/page.tsx
@@ -1,12 +1,51 @@
+"use client";
+
 import { Flashcard } from "@/components/flashcards/Flashcard";
 import { hiraganaList } from "@/lib/hiragana";
+import { useState } from "react";
 
 export default function Lesson2() {
+  const [isFlashcardActive, setFlashcardActive] = useState(false);
+
   return (
     <>
       <h1>Hiragana Flashcards 1</h1>
 
-      <Flashcard kanas={hiraganaList.slice(0, 5)} />
+      {!isFlashcardActive && (
+        <>
+          <p>
+            This first flashcard exercise from Japanly covers the five first
+            hiragana seen in the previous lesson:{" "}
+            {hiraganaList
+              .slice(0, 5)
+              .map((hiragana) => hiragana.value)
+              .join(", ")}
+            .
+          </p>
+
+          <p>
+            Fashcard exercises consist of 15 rounds, where you&apos;ll have to
+            select the correct romaji value for a given hiragana, or the other
+            way around.
+          </p>
+
+          <p>
+            At the end of the last round, we&apos;ll give you feedback about
+            which hiragana you should review.
+          </p>
+
+          <p>When you feel ready to start, click on the button below:</p>
+
+          <div
+            className="border rounded p-2 w-fit cursor-pointer"
+            onClick={() => setFlashcardActive((prev) => !prev)}
+          >
+            Start
+          </div>
+        </>
+      )}
+
+      {isFlashcardActive && <Flashcard kanas={hiraganaList.slice(0, 5)} />}
     </>
   );
 }
